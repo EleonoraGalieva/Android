5916d48626afa7fe007800e311ea7ed79c7c7f87
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-12-27 22:47:34 +0300

    game view added

--- Game/app/src/main/java/com/example/game/GameActivity.java
+++ Game/app/src/main/java/com/example/game/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void onLetterTouched(View view){
+
     }
 }
